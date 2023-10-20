1c1a8ac108747d603fdd0760b7fd248317bef082
soltanifarah <soltanifarah.sl@gmail.com>
2023-10-20 05:23:30 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,49 +1,58 @@
-#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 
-/**
- * _printf - prints anything
- * @format: the format string
- *
- * Return: number of bytes printed
- */
 int _printf(const char *format, ...)
 {
-	int sum = 0;
-	va_list ap;
-	char *x, *start;
-	params_t params = PARAMS_INIT;
+    va_list args;
+    int count = 0; // To keep track of the number of characters printed
 
-	va_start(ap, format);
+    va_start(args, format);
 
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (x = (char *)format; *x; x++)
-	{
-		init_params(&params, ap);
-		if (*x != '%')
-		{
-			sum += _putchar(*x);
-			continue;
-		}
-		start = x;
-		x++;
-		while (get_flag(x, &params)) /* while char at p is flag char */
-		{
-			x++; /* next char */
-		}
-		x = get_width(x, &params, ap);
-		x = get_precision(x, &params, ap);
-		if (get_modifier(x, &params))
-			x++;
-		if (!get_specifier(x))
-			sum += print_from_to(start, x,
-				params.l_modifier || params.h_modifier ? x - 1 : 0);
-		else
-			sum += get_print_func(x, ap, &params);
-	}
-	_putchar(BUF_FLUSH);
-	va_end(ap);
-	return (sum);
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++; // Move to the character after '%'
+
+            if (*format == 'c')
+            {
+                // Handle the 'c' conversion specifier
+                char c = va_arg(args, int); // Get the character argument
+                write(1, &c, 1); // Print the character to stdout
+                count++;
+            }
+            else if (*format == 's')
+            {
+                // Handle the 's' conversion specifier
+                char *str = va_arg(args, char *); // Get the string argument
+                while (*str)
+                {
+                    write(1, str, 1); // Print each character of the string
+                    str++;
+                    count++;
+                }
+            }
+            else if (*format == '%')
+            {
+                // Handle the '%%' conversion specifier
+                write(1, "%", 1); // Print a literal '%' character
+                count++;
+            }
+            else
+            {
+                // Unsupported conversion specifier, skip it
+            }
+        }
+        else
+        {
+            write(1, format, 1); // Print regular characters
+            count++;
+        }
+
+        format++;
+    }
+
+    va_end(args);
+    return count;
 }
+
