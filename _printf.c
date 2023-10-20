32cf3fcbc057669fdd524709cc2d54b3a15e2257
soltanifarah <soltanifarah.sl@gmail.com>
2023-10-20 05:26:41 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,58 +1,94 @@
+#include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
 
+#define BUFF_SIZE 1024
+
+int handle_format_specifier(const char *format, int *i, va_list list, char buffer[], int *buff_ind);
+void print_char(char c, char buffer[], int *buff_ind);
+void print_string(char *str, char buffer[], int *buff_ind);
+
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0; // To keep track of the number of characters printed
+    if (format == NULL)
+        return -1;
 
-    va_start(args, format);
+    va_list list;
+    va_start(list, format);
 
-    while (*format)
+    int printed_chars = 0;
+    char buffer[BUFF_SIZE] = {'\0'};
+    int buff_ind = 0;
+
+    for (int i = 0; format[i] != '\0'; i++)
     {
-        if (*format == '%')
+        if (format[i] != '%')
         {
-            format++; // Move to the character after '%'
-
-            if (*format == 'c')
+            buffer[buff_ind++] = format[i];
+            if (buff_ind == BUFF_SIZE)
             {
-                // Handle the 'c' conversion specifier
-                char c = va_arg(args, int); // Get the character argument
-                write(1, &c, 1); // Print the character to stdout
-                count++;
+                write(1, buffer, buff_ind);
+                buff_ind = 0;
             }
-            else if (*format == 's')
-            {
-                // Handle the 's' conversion specifier
-                char *str = va_arg(args, char *); // Get the string argument
-                while (*str)
-                {
-                    write(1, str, 1); // Print each character of the string
-                    str++;
-                    count++;
-                }
-            }
-            else if (*format == '%')
-            {
-                // Handle the '%%' conversion specifier
-                write(1, "%", 1); // Print a literal '%' character
-                count++;
-            }
-            else
-            {
-                // Unsupported conversion specifier, skip it
-            }
+            printed_chars++;
         }
         else
         {
-            write(1, format, 1); // Print regular characters
-            count++;
+            if (handle_format_specifier(format, &i, list, buffer, &buff_ind) == -1)
+            {
+                va_end(list);
+                return -1; // Invalid format specifier
+            }
+            printed_chars += 1; // Count '%' as a printed character
         }
-
-        format++;
     }
 
-    va_end(args);
-    return count;
+    write(1, buffer, buff_ind);
+    va_end(list);
+    return printed_chars;
 }
 
+int handle_format_specifier(const char *format, int *i, va_list list, char buffer[], int *buff_ind)
+{
+    char c = format[*i];
+    switch (c)
+    {
+        case 'c':
+            print_char(va_arg(list, int), buffer, buff_ind);
+            break;
+        case 's':
+            print_string(va_arg(list, char *), buffer, buff_ind);
+            break;
+        case '%':
+            print_char('%', buffer, buff_ind);
+            break;
+        // Add more format specifiers here
+        default:
+            return -1; // Invalid format specifier
+    }
+    return 0;
+}
+
+void print_char(char c, char buffer[], int *buff_ind)
+{
+    buffer[(*buff_ind)++] = c;
+    if (*buff_ind == BUFF_SIZE)
+    {
+        write(1, buffer, BUFF_SIZE);
+        *buff_ind = 0;
+    }
+}
+
+void print_string(char *str, char buffer[], int *buff_ind)
+{
+    while (*str != '\0')
+    {
+        buffer[(*buff_ind)++] = *str;
+        if (*buff_ind == BUFF_SIZE)
+        {
+            write(1, buffer, BUFF_SIZE);
+            *buff_ind = 0;
+        }
+        str++;
+    }
+}
